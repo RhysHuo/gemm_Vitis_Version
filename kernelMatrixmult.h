e3e813317d165e2bb9ef5f86dd48e5ea0277fa3a
RhysHuo <RhysHuo_1120272267@163.com>
2022-06-30 15:41:55 +0100

    Add files via upload

---
+++ kernelMatrixmult.h
@@ -0,0 +1,16 @@
+#ifndef KERNELMATRIXMULT_H_
+#define KERNELMATRIXMULT_H_
+#include "matrix_mult.h"
+
+int kernelMatrixmult1(
+ap_uint<2> ternary,
+DTYPE *array_a,
+DTYPE *array_b,
+DTYPE_OUT *array_c,
+int M,
+int P,
+int begin,
+int end);
+
+
+#endif 
