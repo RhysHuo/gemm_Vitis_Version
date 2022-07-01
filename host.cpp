dcc5202f4525dcb976d851142082a3d9287e64a8
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-01 15:55:40 +0100

    Update host.cpp

--- host.cpp
+++ host.cpp
@@ -27,7 +27,8 @@ static void init_arrays(DTYPE *B, DTYPE_OUT *C_sw, DTYPE_OUT *C)
     std::cout << "boundary (out of the first loop)"<< B[(SM - 1) * SP + (SP - 1)] << std::endl;
     for (int i = 0; i < SN; i++) {
         for (int j = 0; j < SP; j++) {
-			C_sw[i * SP + j] = 0x00;
+			std::cout << "C_sw "<< i * SP + j << " " << C_sw[i * SP + j] << std::endl;
+			C_sw[i * SP + j] = 0x0;
 			std::cout << "C_sw "<< i * SP + j << " " << C_sw[i * SP + j] << std::endl;
 			C[i * SP + j] = 0;
 		//std::cout << "C_sw "<< i * SP + j << " " << C_sw[i * SP + j] << std::endl;
