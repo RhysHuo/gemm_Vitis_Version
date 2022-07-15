e315d9fa99a5d3fe949923e467e2b1af12517e85
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-15 13:05:30 +0100

    Delete Makefile.mk

--- Makefile.mk
+++
@@ -1,8 +0,0 @@
-rm host.cpp
-git clone https://github.com/RhysHuo/gemm_Vitis_Version.git
-cd gemm_Vitis_Version
-cp host.cpp ..
-cd ..
-rm -rf gemm_Vitis_Version
-g++ -c -I${XILINX_XRT}/include -I/mnt/scratch/rhyhuo/HLS_arbitrary_Precision_Types/include -g -o host.o host.cpp
-g++ -g -L${XILINX_XRT}/lib/ -I/mnt/scratch/rhyhuo/HLS_arbitrary_Precision_Types/include -o host host.o -lOpenCL -lpthread -lrt -lstdc++
