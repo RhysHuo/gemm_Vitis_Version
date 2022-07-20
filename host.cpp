f95f36353f0d8eef1160cc2a1bcd2c5f0c2867bb
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-20 15:04:56 +0100

    Update host.cpp

--- host.cpp
+++ host.cpp
@@ -498,7 +498,11 @@ int main(int argc, char* argv[]) {
     if (result_check(array_c, array_c_sw))
         return 1;
 	
-	for(int k = 0; k < 50; k++)
+	for(int k = 0; k < 64; k++)
+		std::cout << "array_a = " << k << array_a[k] << std::endl;
+	
+	
+	for(int k = 0; k < 64; k++)
 		std::cout << "array_c_sw = " << k << " " << array_c_sw[k] << std::endl;
 	
 	std::chrono::duration<double> fpga_duration = fpga_end - fpga_begin;
